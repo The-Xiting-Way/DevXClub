48859d1cfe6d2757170392fe75fbdc2f2268692e
Aryainguz <aryaninguz369@gmail.com>
2024-02-26 13:33:58 +0530

    feat: Added ContainerScroll and update page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
-import Image from "next/image";
 import Hero from "./components/Hero";
+import { HoverEffect } from "./components/HeroCards";
 import { Navbar } from "./components/Navbar";
+import { ScrollSection } from "./components/ScrollSection";
+import Numbers from "./components/Stats";
+import HowItWorks from "./components/Steps";
+import Testimonial from "./components/Testimonial";
 
 export default function Home() {
   return (
    <>
    <Navbar/>
    <Hero/>
+   <HoverEffect/>
+   <HowItWorks/>
+   {/* <Testimonial/> */}
+   <ScrollSection/>
+   <Numbers/>
    </>
   );
 }
