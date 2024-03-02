b8a0c29015e5bc137327d04c28bc43526f38ed3e
Sahil <Sahil2003@gmail.com>
2024-03-03 03:08:51 +0530

    Fix : Typo Error Fixed

--- app/signIn/page.tsx
+++ app/signIn/page.tsx
@@ -56,10 +56,10 @@ const SignIn = () => {
                 <div className='absolute top-0 right-0 bg-transparent h-auto w-auto overflow-hidden'>
                     <WavyBackground className="max-w-4xl bg-transparent mx-auto pb-40">
                         <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
-                            Hero waves are cool
+                            Hardik Swaagat hai
                         </p>
                         <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
-                            Leverage the power of canvas to create a beautiful hero section
+                            Leverage the power of Community of coders who programs reality
                         </p>
                     </WavyBackground>
                 </div>
