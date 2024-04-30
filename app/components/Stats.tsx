0a45072060ed2fd01a39c323b95433ce5531c5d9
Gurpreet0798 <gurpreetkaur07988@gmail.com>
2024-05-01 01:24:25 +0530

    pushed DevXClub Website

--- app/components/Stats.tsx
+++ app/components/Stats.tsx
@@ -1,14 +1,15 @@
 
 import React from 'react';
 
-const Numbers = () => {
+const Numbers: React.FC = () => {
     return (
         <section className="py-10 bg-gradient-to-r from gray-00 to-gray-200 sm:py-16 lg:py-24">
             <div className="max-w-5xl px-4 mx-auto sm:px-6 lg:px-8">
-                <div className="max-w-2xl mx-auto text-center">
-                    <h2 className="text-3xl font-bold leading-tight text-gray-300 sm:text-4xl lg:text-5xl">Numbers tell our story</h2>
-                    <p className="mt-3 text-xl leading-relaxed text-gray-500 md:mt-8">We have many who are like us and like you.</p>
-                </div>
+
+                                <div className="max-w-2xl mx-auto text-center">
+                                    <h2 className="text-3xl font-bold leading-tight text-gray-300 sm:text-4xl lg:text-5xl">Numbers tell our story</h2>
+                                    <p className="mt-3 text-xl leading-relaxed text-gray-500 md:mt-8">We have many who are like us and like you.</p>
+                                </div>
 
                 <div className="grid grid-cols-1 gap-8 mt-10 text-center lg:mt-24 sm:gap-x-8 md:grid-cols-3">
                     <div>
