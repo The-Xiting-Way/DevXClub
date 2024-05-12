49a59ad2d5f331641f1ad9d5e052b0f27ed52037
Roni Sarkar <ronichandrasarkar@gmail.com>
2024-05-12 17:14:02 +0530

    improved bg

--- app/components/ContactUs.tsx
+++ app/components/ContactUs.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import { BackgroundBeams } from "./ui/background-beams";
 import { motion } from "framer-motion";
 
 const ContactUs = () => {
@@ -8,7 +7,7 @@ const ContactUs = () => {
     // Coming soon
   };
   return (
-    <div className="h-[40rem] w-full rounded-md bg-neutral-950 relative flex flex-col items-center justify-center antialiased">
+    <div className="h-[40rem] w-full rounded-md relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-2xl mx-auto p-4">
         <motion.div
           initial={{
@@ -64,7 +63,18 @@ const ContactUs = () => {
           <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-mediumVioletRed-500 to-transparent" />
         </button>
       </motion.div>
-      <BackgroundBeams />
+      <div
+        className="absolute inset-x-0 top-[calc(100%-19rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-43rem)]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+      </div>
     </div>
   );
 };
